16169d2558e14e2f1e77407fe16a578b5c0c54ab
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-17 09:04:03 +0700

    Using primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { ToolbarModule } from 'primeng/primeng';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MarketingPageComponent } from './marketing-page/marketing-page.component';
+import { MarketingMenuBarComponent } from './marketing-menu-bar/marketing-menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketingPageComponent,
+    MarketingMenuBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/marketing-menu-bar/marketing-menu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ToolbarModule } from 'primeng/primeng';
+@Component({
+  selector: 'app-marketing-menu-bar',
+  templateUrl: './marketing-menu-bar.component.html',
+  styleUrls: ['./marketing-menu-bar.component.css']
+})
+export class MarketingMenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
