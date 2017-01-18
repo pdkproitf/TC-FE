8b2f5ac6f1f9039790d55a83aba19ec20b31dfff
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-18 16:29:53 +0700

    edit caroulse

--- src/app/project-field-list/project-field-list.component.ts
+++ src/app/project-field-list/project-field-list.component.ts
@@ -23,6 +23,30 @@ export class ProjectFieldListComponent implements OnInit {
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 1',
       color: '#EA4335',
+    },
+    {
+      name: 'Temp Project',
+      tasks: ['Task1', 'Task2', 'Task3'],
+      company: 'My Company',
+      color: '#EA4335',
+    },
+    {
+      name: 'Temp Project 1',
+      tasks: ['Task1', 'Task2', 'Task3'],
+      company: 'My Company 1',
+      color: '#EA4335',
+    },
+    {
+      name: 'Temp Project',
+      tasks: ['Task1', 'Task2', 'Task3'],
+      company: 'My Company',
+      color: '#EA4335',
+    },
+    {
+      name: 'Temp Project 1',
+      tasks: ['Task1', 'Task2', 'Task3'],
+      company: 'My Company 1',
+      color: '#EA4335',
     }
   ]
   ngOnInit() {
