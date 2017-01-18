7d07f9a04feb4bc6539968837fa0e993f45f95f3
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-18 15:53:01 +0700

    research interact between components

--- src/app/project-field-list/project-field-list.component.ts
+++ src/app/project-field-list/project-field-list.component.ts
@@ -1,3 +1,4 @@
+import { ProjectFieldComponent } from './../project-field/project-field.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-field-list.component.scss']
 })
 export class ProjectFieldListComponent implements OnInit {
-
+  sharedVarParent = 'hello';
   constructor() { }
 
   ngOnInit() {
--- src/app/project-field/project-field.component.ts
+++ src/app/project-field/project-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Project } from '../models/project';
 @Component({
   selector: 'app-project-field',
@@ -13,11 +13,31 @@ export class ProjectFieldComponent implements OnInit {
     color: '#EA4335',
   };
   classBtns = ['playBtn', 'playBtn', 'playBtn'];
+  private _sharedVar = '';
+  @Input()
+  set sharedVar(name: string){
+    this._sharedVar = name;
+    console.log('Changed');
+  }
+  get sharedVar(){
+    return this._sharedVar;
+  }
+  @Output() sharedVarChange = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
-  changeClass(i): void{
+  changeClass(i): void {
+    for (let j = 0; j < 3; j++) {
+      if (j !== i) {
+        this.classBtns[j] = 'playBtn';
+      }
+    }
     this.classBtns[i] = this.classBtns[i] === 'playBtn' ? 'stopBtn' : 'playBtn';
+  }
+  change(newValue) {
+    console.log('newvalue', newValue);
+    this._sharedVar = newValue;
+    this.sharedVarChange.emit(newValue);
   }
 }
