9b925d2540d699135842d851160f0f93df7145d4
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-18 15:07:07 +0700

    create UI for the project list

--- src/app/project-field/project-field.component.ts
+++ src/app/project-field/project-field.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Project } from '../models/project';
 @Component({
   selector: 'app-project-field',
   templateUrl: './project-field.component.html',
   styleUrls: ['./project-field.component.scss']
 })
 export class ProjectFieldComponent implements OnInit {
-
+  myProject: Project = {
+    name: 'Temp Project',
+    tasks: ['Task1', 'Task2', 'Task3'],
+    company: 'My Company',
+    color: '#EA4335',
+  };
+  classBtns = ['playBtn', 'playBtn', 'playBtn'];
   constructor() { }
 
   ngOnInit() {
   }
-
+  changeClass(i): void{
+    this.classBtns[i] = this.classBtns[i] === 'playBtn' ? 'stopBtn' : 'playBtn';
+  }
 }
