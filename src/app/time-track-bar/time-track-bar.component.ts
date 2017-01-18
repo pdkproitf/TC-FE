d69f403b573e8430077a0ae837b0e057306ef14f
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-18 11:46:08 +0700

    change icon button when click

--- src/app/time-track-bar/time-track-bar.component.ts
+++ src/app/time-track-bar/time-track-bar.component.ts
@@ -7,8 +7,11 @@ import { AutoCompleteModule, InputTextModule, ButtonModule } from 'primeng/prime
   styleUrls: ['./time-track-bar.component.scss']
 })
 export class TimeTrackBarComponent implements OnInit {
+  classBtn: String = 'playBtn';
   constructor() { }
-
   ngOnInit() {
+  }
+  changeClass(): void {
+    this.classBtn = this.classBtn === 'playBtn' ? 'stopBtn' : 'playBtn';
   }
 }
