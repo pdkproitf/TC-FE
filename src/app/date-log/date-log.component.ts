1dd705fa064acd4c99c1b95cf4f1d99e365dad33
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-19 17:55:59 +0700

    add days in week UI

--- src/app/date-log/date-log.component.ts
+++ src/app/date-log/date-log.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-log.component.scss']
 })
 export class DateLogComponent implements OnInit {
-
+  days: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Total'];
+  time: string[] = ['8:00', '8:00', '8:00', '8:00', '8:00', '0:00', '0:00', '40:00'];
   constructor() { }
 
   ngOnInit() {
