756cc5a6a60e112c9385ae142f59113e40921dfe
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-19 11:52:26 +0700

    Edit router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { MarketingPageComponent } from './marketing-page/marketing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -6,7 +8,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     { path: 'sign-up', component: SignUpComponent },
-    { path: 'sign-in', component: SignInComponent }
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', component: MarketingPageComponent }
     // { path: '', component: AppComponent }
     // { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,3 +1,5 @@
+import { DashboardComponent } from './../dashboard/dashboard.component';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  signIn(): void{
+    this.router.navigateByUrl('/dashboard');
+  }
 }
