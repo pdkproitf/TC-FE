4a6de3dd1e8e61f4b52d0e8ea78a8c909c93e665
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-01-20 11:41:00 +0700

    add submit function

--- src/app/project-field-list/project-field-list.component.ts
+++ src/app/project-field-list/project-field-list.component.ts
@@ -14,37 +14,37 @@ export class ProjectFieldListComponent implements OnInit {
       name: 'Temp Project',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company',
-      color: '#EA4335',
+      color: '#1abc9c',
     },
     {
       name: 'Temp Project 1',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 1',
-      color: '#0098F0',
+      color: '#2ecc71',
     },
     {
       name: 'Temp Project 2',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 2',
-      color: '#58E573',
+      color: '#3498db',
     },
     {
       name: 'Temp Project 3',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 3',
-      color: '#FFB91B',
+      color: '#f1c40f',
     },
     {
       name: 'Temp Project 4',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 4',
-      color: '#A82AB5',
+      color: '#d35400',
     },
     {
       name: 'Temp Project 5',
       tasks: ['Task1', 'Task2', 'Task3'],
       company: 'My Company 5',
-      color: '#EA4335',
+      color: '#e74c3c',
     }
   ]
   ngOnInit() {
