60ab17238240e974757d20f3461a18610ebf1668
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-02-07 08:53:52 +0700

    reedit model for testing purpose

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,6 +1,9 @@
 export class Project {
     name: string;
-    client_id: number;
-    background: string;
-    report_permission: number;
+    //client_id: number;
+    //background: string;
+    //report_permission: number;
+    tasks: string[];
+    color: string;
+    company: string;
 }
