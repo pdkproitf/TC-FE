e2f6f8d3a6c0b556e1a166e6bf9845605aa50ffe
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-02-08 15:53:54 +0700

    edit project model to add more fields

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,10 +1,33 @@
+export class MemberRole {
+    user_id: number;
+    role_id: number;
+}
+
+export class Member {
+    user_id: number;
+}
+
+export class Category {
+    category_id: number;
+    members: Member[] = [];
+    billable: boolean;
+}
+
+export class CategoryMembers {
+    existing: Category[] = [];
+    new_one: Category[] = [];
+}
+
 export class Project {
     name: string;
     client_id: number;
     background: string;
     report_permission: number;
-    tasks: string[];
+    tasks: string[] = [];
+    member_roles: MemberRole[] = [];
+    category_members: CategoryMembers;
 }
+
 export class ProjectPost {
     project: Project;
 }
