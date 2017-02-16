de16a809931c1092aa82cf16131d341db51a577b
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-02-16 16:17:03 +0700

    fix choosing item from dropdown

--- src/app/adding-member/adding-member.component.ts
+++ src/app/adding-member/adding-member.component.ts
@@ -35,11 +35,12 @@ export class AddingMemberComponent implements OnInit {
   }
 
   addEmployee(employee: Employee) {
-    this.employees.push(employee);
-    let member = new Member;
-    member.user_id = employee.id;
-    this.onAdd.emit(member);
-    this.undisplayDiv();
+    if (this.employees.indexOf(employee) < 0) {
+      this.employees.push(employee);
+      let member = new Member;
+      member.user_id = employee.id;
+      this.onAdd.emit(member);
+    }
   }
 
   removeEmployee(i) {
