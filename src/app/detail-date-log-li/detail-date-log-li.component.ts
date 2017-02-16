e958b4632dbbf892f940e6c9d40a3ded79cb8c3b
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-02-16 10:35:41 +0700

    tracking time and fetching time to table below

--- src/app/detail-date-log-li/detail-date-log-li.component.ts
+++ src/app/detail-date-log-li/detail-date-log-li.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { TimerFetch } from './../models/timer-fetch';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-detail-date-log-li',
@@ -7,14 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailDateLogLiComponent implements OnInit {
   spanClass = 'hidden';
+  _timerFetch: TimerFetch = new TimerFetch();
+  @Input()
+  set timerFetch(value) {
+    this._timerFetch = value;
+    this.totalTime();
+    this.from = this.convertTime(this.timerFetch.start_time);
+    this.to = this.convertTime(this.timerFetch.stop_time);
+  }
+  get timerFetch() {
+    return this._timerFetch;
+  }
+  from;
+  to;
+  total;
   constructor() { }
 
   ngOnInit() {
   }
+
   showSpan() {
     this.spanClass = 'aaa';
   }
+
   hideSpan() {
     this.spanClass = 'hidden';
+  }
+
+  convertTime(dateString) {
+    let date = new Date(dateString);
+    let hourString = date.getHours().toString();
+    let minuteString = date.getMinutes().toString();
+    let res = hourString + ':' + minuteString;
+    return res;
+  }
+
+  totalTime() {
+    let from = new Date(this.timerFetch.start_time).getTime();
+    let to = new Date(this.timerFetch.stop_time).getTime();
+    this.total = (to - from) / 1000;
   }
 }
