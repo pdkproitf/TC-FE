b1880d12237a563034053b3b95560c90a34d094f
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-03-09 12:06:52 +0700

    div for setting time in report

--- src/app/report-search/report-search.component.ts
+++ src/app/report-search/report-search.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-search.component.scss']
 })
 export class ReportSearchComponent implements OnInit {
-
+  options: string[][] = [['Yesterday', 'Last week', 'Last month', 'Last year'],
+  ['Today', 'This week', 'This month', 'This year']];
+  classDiv = ['choose-time'];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeClassDiv(i) {
+    if (i === 0) {
+      this.classDiv[i] = (this.classDiv[i] === 'choose-time') ? 'choose-time hide' : 'choose-time';
+    }
   }
 
 }
