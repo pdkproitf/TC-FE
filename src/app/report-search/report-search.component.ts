bb70af97329d07a14874113bc7f8ad8a7684607b
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-03-09 14:13:27 +0700

    finish UI report main page

--- src/app/report-search/report-search.component.ts
+++ src/app/report-search/report-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ProjectGetAll } from './../models/project';
+import { Member } from './../models/member';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-report-search',
@@ -8,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class ReportSearchComponent implements OnInit {
   options: string[][] = [['Yesterday', 'Last week', 'Last month', 'Last year'],
   ['Today', 'This week', 'This month', 'This year']];
-  classDiv = ['choose-time'];
+  @Input()
+  members: Member[] = [];
+  @Input()
+  projectLists: ProjectGetAll[]= [];
+  classDiv = ['choose-time hide', 'choose-project hide', 'choose-member hide'];
   constructor() { }
 
   ngOnInit() {
@@ -18,6 +24,19 @@ export class ReportSearchComponent implements OnInit {
     if (i === 0) {
       this.classDiv[i] = (this.classDiv[i] === 'choose-time') ? 'choose-time hide' : 'choose-time';
     }
+  }
+
+  showClassDiv(i) {
+    if (i === 1) {
+      this.classDiv[i] = 'choose-project';
+    }
+    if (i === 2) {
+      this.classDiv[i] = 'choose-member';
+    }
+  }
+
+  hideClassDiv(i) {
+    this.classDiv[i] = this.classDiv[i] + ' hide';
   }
 
 }
