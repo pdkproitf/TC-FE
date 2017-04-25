88b23098d26aca4d0619aa55d1b0ef2368676448
Võ Đức Hồng <vo.duc.hong94@gmail.com>
2017-04-25 09:22:51 +0700

    funny quotes

---
+++ src/app/manage-dashboard/funny-quotes/funny-quotes.component.ts
@@ -0,0 +1,17 @@
+import { QuotesService } from './../../services/quotes-service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funny-quotes',
+  templateUrl: './funny-quotes.component.html',
+  styleUrls: ['./funny-quotes.component.scss']
+})
+export class FunnyQuotesComponent implements OnInit {
+  content: string = '';
+  constructor(private quotesService: QuotesService) { }
+
+  ngOnInit() {
+    this.content = this.quotesService.generateQuote();
+  }
+
+}
